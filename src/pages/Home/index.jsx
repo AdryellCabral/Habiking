2480b9c1f465e7b6dfa9693c882957b38619616b
Adryell Cabral <8111841-AdryellCabral@users.noreply.gitlab.com>
2021-06-09 20:57:56 -0300

    💄 style: homepage base styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,2 +1,34 @@
-const HomePage = () => <div>home page aquiiiii</div>;
+import {
+  DivContainerStyled,
+  DivContainer2Styled,
+  DivContainer3Styled,
+  DivContainer4Styled,
+  PStyled,
+  ButtonStyled,
+  TitleStyled,
+} from "./styles";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <DivContainerStyled>
+    <DivContainer2Styled>
+      <DivContainer4Styled color="#c8bfc7">
+        <TitleStyled>Gestor de Hábitos</TitleStyled>
+        <PStyled>
+          Gestor de hábitos é uma aplicação onde você pode gerir seu lifestyle,
+          tanto sozinho quanto em grupos que você pode entrar ou criar novos.
+        </PStyled>
+      </DivContainer4Styled>
+      <DivContainer3Styled color="#18206F">
+        <ButtonStyled>Cadastre-se</ButtonStyled>
+        <div>
+          <PStyled color="white">Já possui uma conta?</PStyled>
+          <Link to="/login">
+            <ButtonStyled>Login</ButtonStyled>
+          </Link>
+        </div>
+      </DivContainer3Styled>
+    </DivContainer2Styled>
+  </DivContainerStyled>
+);
 export default HomePage;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const DivContainerStyled = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(#485563 20%, #29323c 75%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DivContainer2Styled = styled.div`
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+  height: 85%;
+`;
+
+export const DivContainer3Styled = styled.div`
+  background-color: ${(props) => `${props.color}`};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 50%;
+  text-align: center;
+  justify-content: space-around;
+`;
+
+export const PStyled = styled.p`
+  font-size: 20px;
+  padding: 0 20px;
+  color: ${(props) => `${props.color}`};
+  text-decoration: none;
+
+  a {
+    color: inherit;
+  }
+`;
+
+export const TitleStyled = styled.h3`
+  font-size: 30px;
+  color: black;
+  font-family: "Roboto", sans-serif;
+`;
+
+export const ButtonStyled = styled.button`
+  background-color: yellow;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 10px 20px;
+`;
+
+export const DivContainer4Styled = styled(DivContainer3Styled)`
+  justify-content: space-around;
+`;
