40724dfbeebdfbdd7eccbc8133e2f52299272f38
Kevin Gomes <lederoon@outlook.com>
2021-06-09 12:38:38 -0300

    feat: button component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import  Routes  from "./routes";
+import Button from "./components/Button";
+import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <Routes />
       <GlobalStyle />
     </div>
