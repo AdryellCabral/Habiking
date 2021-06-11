1966c4fccfe663bea4f60b770f27a33ccf824908
Marcos Vinicius P. Vieira <mviniciuspvieira@hotmail.com>
2021-06-10 21:17:26 -0300

    feat: added group info and users info

--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -3,20 +3,38 @@ import ButtonComp from "../../components/ButtonComp";
 import GroupList from "../../components/GroupList";
 import NavMenu from "../../components/NavMenu";
 import { useGroupsSubscriptions } from "../../providers/groupsSubscriptions";
-import { DivContainer } from "./styles";
+import { DivContainer, DivGroup } from "./styles";
 
 const GroupsPages = () => {
-
-  const {groups} = useGroupsSubscriptions()
-  const [showGroup, setShowGroup] = useState([])
+  const { groups } = useGroupsSubscriptions();
+  const [showGroup, setShowGroup] = useState([]);
+  const { name, category, description, creator, users_on_group } = showGroup;
   return (
     <>
       <NavMenu />
       <DivContainer>
-          <ButtonComp>Criar Novo Grupo</ButtonComp>
-          <ButtonComp>Buscar Novo Grupo</ButtonComp>
+        <ButtonComp>Criar Novo Grupo</ButtonComp>
+        <ButtonComp>Buscar Novo Grupo</ButtonComp>
 
-          <GroupList setShowGroup={setShowGroup} groups={groups} />
+        <GroupList setShowGroup={setShowGroup} groups={groups} />
+        <DivGroup>
+          <div id="group-info">
+            <h1>Grupo</h1>
+            <h2>Nome: {name}</h2>
+            <h2>Categoria: {category}</h2>
+            <p>Descrição: {description}</p>
+          </div>
+
+          <div id="users">
+            <h3>Usuários</h3>
+            <ul>
+              <li id="creator">{creator?.username}</li>
+              {users_on_group?.map((user, index) => (
+                <li key={index}>{user.username}</li>
+              ))}
+            </ul>
+          </div>
+        </DivGroup>
       </DivContainer>
     </>
   );
--- src/pages/Groups/styles.js
+++ src/pages/Groups/styles.js
@@ -3,12 +3,34 @@ import styled from "styled-components";
 export const DivContainer = styled.div`
   width: 80%;
   margin: 0 auto;
-  
+
   && {
     button {
       width: 100%;
-      margin-left:0;
-      margin-right:0;
+      margin-left: 0;
+      margin-right: 0;
     }
+  }
+`;
+
+export const DivGroup = styled.div`
+  div#group-info {
+    h2,p {
+      height: 32px;
+    }
+  }
+h1{
+  font-size:1.5rem;
+}
+  h3 {
+    text-align: center;
+  }
+
+  #users {
+    border: 2px double var(--borderColor);
+    margin-top: 15px;
+    padding: 5px;
+    height: 250px;
+    background-color: whitesmoke;
   }
 `;
