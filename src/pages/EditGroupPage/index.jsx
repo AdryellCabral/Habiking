3928c6d2fb8748d738842b33aa5550cd00311303
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-06-11 15:08:54 -0300

    feature: Created page EditGroupPage.

---
+++ src/pages/EditGroupPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ContainerStyled } from './styles';
+import GroupUpdateForm from '../../components/GroupUpdateForm';
+
+const EditGroupPage = () => {
+  return (
+    <ContainerStyled>
+      <GroupUpdateForm />
+      
+    </ContainerStyled>
+  );
+}
+
+export default EditGroupPage;
