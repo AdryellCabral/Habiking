5e4d3a52af6c1c5e0b87b910c3ba4a713d02c275
Marcos Vinicius P. Vieira <mviniciuspvieira@hotmail.com>
2021-06-14 12:26:45 -0300

    refactor: formated date in cardActivies

--- src/components/CardActivities/index.jsx
+++ src/components/CardActivities/index.jsx
@@ -1,10 +1,13 @@
 const CardActivities = ({activity}) => {
     const {title, realization_time} = activity
+    
+    let data = new Date (realization_time)
+    const newData = `${data.toLocaleDateString('pt-br')} às ${data.toLocaleTimeString('pt-br')}`
     return (
         <div>
             <h3>{title}</h3>
-            <h3>{realization_time}</h3>
-        </div>
+            <h3>{newData}</h3>
+        </div>  
     )
 }
 
