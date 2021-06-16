a86558909ae896082c18dae566728c76922e32fc
Marcos Vinicius P. Vieira <mviniciuspvieira@hotmail.com>
2021-06-15 21:47:11 -0300

    styles: basic styles groupPages

--- src/pages/Groups/styles.js
+++ src/pages/Groups/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const DivContainer = styled.div`
   width: 80%;
@@ -12,8 +12,37 @@ export const DivContainer = styled.div`
     button {
       width: 100%;
       margin-left: 0;
+      padding: 0;
+      height: 50px;
       margin-right: 0;
     }
+  }
+
+  @media (min-width: 768px) {
+    #buttons-container {
+      display: flex;
+      justify-content: space-around;
+      max-width: 800px;
+      margin: 0 auto;
+      button {
+        width: 250px;
+        margin: 15px 0;
+      }
+    }
+
+    #group-container {
+      display: flex;
+
+      #group-list {
+        width: 30%;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 100%;
+    padding-left: 30px;
+    padding-right: 10px;
   }
 `;
 
@@ -27,8 +56,12 @@ export const DivGroup = styled.div`
   h1 {
     font-size: 1.5rem;
   }
-  h3 {
-    text-align: center;
+  
+
+  && {
+    div > a > button {
+      height: 40px;
+    }
   }
 
   .infos {
@@ -39,12 +72,52 @@ export const DivGroup = styled.div`
     background-color: whitesmoke;
     overflow: auto;
     overflow-x: hidden;
+    > h3 {
+    text-align: center;
+  }
   }
 
   #users {
-    li {
+    li#creator {
       padding: 5px;
       border: 2px double var(--borderColor);
     }
   }
+
+  @media (min-width: 768px) {
+    padding-left: 25px;
+    width: 100%;
+    #cards-container {
+      display: flex;
+
+      div {
+        flex-basis: 33.33%;
+      }
+    }
+
+    .infos {
+      height: 340px;
+      margin: 0 5px;
+    }
+    #edit-group {
+      text-align: center;
+    }
+    && {
+      div > a > button {
+        margin: 0;
+        margin-top: 10px;
+        width: 200px;
+      }
+    }
+  }
+`;
+
+export const Li = styled.li`
+  padding: 5px;
+  border-radius: 2px;
+  ${(props) =>
+    props.index % 2 === 0 &&
+    css`
+      background-color: lightgray;
+    `}
 `;
