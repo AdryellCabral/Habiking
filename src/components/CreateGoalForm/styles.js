712f9778d3a25305dd03c79933656c2efe1dfa91
Thiago Henrique Dias Silva <professorthiagohenriqueds@gmail.com>
2021-06-16 11:29:14 -0300

    style: Modified width, height and margin in FormStyled elements. EditGroupPage reset.

--- src/components/CreateGoalForm/styles.js
+++ src/components/CreateGoalForm/styles.js
@@ -2,10 +2,33 @@ import styled from 'styled-components';
 
 export const FormStyled = styled.form`
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 80%;
-    margin: 15px auto;
+    margin: 40px auto;
+    align-self: center;
+    width: 90%;
     border: 2px double var(--borderColor);
     border-radius: 10px;
+    color: var(--textColor);
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-height: 600px;
+
+    >div {
+        width: 210px;
+        height: 40px;
+        margin: 15px 0;
+    }
+
+    @media (min-width:768px){
+        width: 100%;
+        border-right: none;
+        border-left: none;
+        border-radius: 0;
+
+        >div {
+            width: 400px;
+            height: 50px;    
+        }
+    }
 `
--- src/pages/EditGroupPage/styles.js
+++ src/pages/EditGroupPage/styles.js
@@ -3,9 +3,11 @@ import { Container } from '@material-ui/core';
 
 export const ContainerStyled = styled(Container)`
     &&{
-       h2{
+        margin: 0;
+        padding: 0;
+        h2{
            margin-top: 20px;
            text-align: center;
-       } 
+        } 
     }
 `
