d9be9fe156554e5e366361fb9b4bd00ff88e34ca
Kevin Gomes <lederoon@outlook.com>
2021-06-17 16:21:02 -0300

    style: finished

--- src/components/CardGroup/styles.js
+++ src/components/CardGroup/styles.js
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 
 export const Infos = styled.div`
-  border: 2px solid var(--colorTwo);
   margin: 20px;
+  border-bottom: 2px solid var(--colorTwo);
   box-shadow: 0 0 4px var(--colorOne);
   background-color: var(--colorThree);
   border-radius: 6px;
 
   @media (min-width: 768px) {
     width: 30vw;
+
+    button {
+      height: 30px;
+    }
   }
 `;
 
 export const GroupContainer = styled.div`
   h1 {
     text-align: center;
-    border-bottom: 2px solid var(--colorTwo);
-    border-radius: 10px;
-    padding: 5px;
     margin: 10px;
     min-width: 70vw;
     color: var(--colorTwo);
@@ -28,9 +29,14 @@ export const GroupContainer = styled.div`
   }
 
   p {
-    margin: 0;
-    margin: 10px;
+    padding: 5px;
     font-size: 1.2rem;
-    color: var(--colorTwo);
+    border-bottom: 2px solid var(--colorTwo);
+    border-radius: 10px;
+    width: 60vw;
+
+    @media (min-width: 768px) {
+      width: 14vw;
+    }
   }
 `;
